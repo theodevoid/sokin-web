400c8d5edacb7bf8105896333c155fbe24e40127
theodevoid <87703322+theodevoid@users.noreply.github.com>
2022-03-23 13:54:05 +0700

    ui: added themes and removed unused styles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,18 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  components: {
+    Text: {
+      baseStyle: {
+        fontFamily: "Nunito Sans"
+      }
+    }
+  }
+})
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
